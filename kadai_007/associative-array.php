5bd2ec3d8f2828d05ef56298545e331ce646d818
gon10222001 <gon10222001@gmail.com>
2024-08-20 21:52:04 +0900

    ディレクトリ"kadai_007"の作成

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP課題007</title>
+</head>
+<body>
+  <P>
+    <?php
+      // 連想配列に値を代入
+      $Array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+      // 連想配列の値を出力
+      print_r($Array);
+    ?>
+  </P>
+</body>
+</html>
